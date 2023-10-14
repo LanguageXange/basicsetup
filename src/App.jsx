45f29342c0588debbe7e423d736db8126dda5a7b
Cindy Lin <hlin0017@seattlecentral.edu>
2023-10-13 19:26:36 -0700

    antialias to true

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Experience from "./Experience";
 function App() {
   const cameraSetting = { zoom: 1.5, near: 0.1, far: 100, position: [3, 5, 3] };
   return (
-    <Canvas shadows={true} gl={{ antialias: false }} camera={cameraSetting}>
+    <Canvas shadows={true} gl={{ antialias: true }} camera={cameraSetting}>
       <Experience />
     </Canvas>
   );
